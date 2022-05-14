55559c86e9a33739a8079c4613c74416df15db27
arandomdood <68734556+arandomdood@users.noreply.github.com>
2022-05-14 13:12:55 +0800

    Update MiniCheat.lua
    - cleaned up code for readability
    - swap task.wait() look for RunService.Heartbeat loop for efficiency
    - changed module.AddScore() to module:AddScore() as it is a method of module
    - changed module:AddScore() behaviour to run CheckKick() even if player entry already exists (unintentional oversight?)
    - added some variables to module to implement RunService.Heartbeat loop

--- MainModule/Modules/MiniCheat.lua
+++ MainModule/Modules/MiniCheat.lua
@@ -1,23 +1,26 @@
-local module = {Storage = {}; ScoreDeducing = true;}
+local RunService = game:GetService("RunService")
+local module = {Storage = {}; ScoreDeducing = true; lastDeduction = tick(); deductionInterval = 120}
 
-function CheckKick(plr:Player)
+local function CheckKick(plr : Player)
 	if module.Storage[plr.UserId][1] >= 10 then
 		plr:Kick("MiniCheat\n\nYou have been kicked for a suspected cheat.\n\nLast Detection:\n"..module.Storage[plr.UserId][2])
 	end
 end
 
-function module.AddScore(user:Player,score:number,reason:string) -- Add scoring to the user
-	if not module.Storage[user.UserId] then 
-		module.Storage[user.UserId] = {score,reason} 
+function module:AddScore(user : Player, score : number, reason : string) -- Add scoring to the user
+	if not self.Storage[user.UserId] then 
+		self.Storage[user.UserId] = {score, reason} 
 		CheckKick(user)
 		return
 	else
-		module.Storage[user.UserId][1] += score
-		module.Storage[user.UserId][2] = reason
+		self.Storage[user.UserId][1] += score
+		self.Storage[user.UserId][2] = reason
+		CheckKick(user)
 		return
 	end
 end
 
+--[[
 task.spawn(function() -- To reduce false positive kicks, create a loop that lowers one's score every 2 minutes. Ineffective, but helpful. Can be toggled.
 	while task.wait(120) do
 		if module.ScoreDeducing then
@@ -27,5 +30,15 @@ task.spawn(function() -- To reduce false positive kicks, create a loop that lowe
 		end
 	end
 end)
+--]]
+
+RunService.Heartbeat:Connect(function()
+	if module.ScoreDeducing and tick() - module.lastDeduction >= module.deductionInterval then
+		for key, value in pairs(module.Storage) do
+			local newValue = math.max(0, module.Storage[key][1] - 1)
+			module.Storage[key][1] = newValue
+		end
+	end
+end
 
 return module
